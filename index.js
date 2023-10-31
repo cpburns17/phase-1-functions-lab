b8e4d7bd92f54d0bd6b4739574e10e1fe7881796
Connor Burns <cpburns17@gmail.com>
2023-10-31 14:20:15 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue) {
+    let distance = Math.abs(42 - someValue);
+    return distance;
+}
+
+function distanceFromHqInFeet(someValue) {
+    let distance = distanceFromHqInBlocks(someValue) * 264;
+        return distance;
+}
+
+function distanceTravelledInFeet(start, end) {
+   let distance = Math.abs(start - end) * 264;
+        return distance;
+}
+
+function calculatesFarePrice(start, end) {
+    let distance = distanceTravelledInFeet(start, end);
+    let price = 0;
+    if (distance <= 400) {
+        return price = 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return price = (distance - 400)* 0.02;
+    }  else if (distance > 2000 && distance <= 2500) {
+        return price = 25;
+    }  else (distance > 2500) 
+        return "cannot travel that far";
+    
+}
